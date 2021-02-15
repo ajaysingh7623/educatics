9d59e33603ef719f4038cc722ec06511892cb177
ajay singh <you@example.com>
2021-02-15 15:43:59 +0530

    edcatics

---
+++ edtech.js
@@ -0,0 +1,13 @@
+function mySubmit() {
+ var value=document.getElementById("myForm").submit();
+  console.log(value);
+}
+
+
+function subscribe() {
+
+ document.getElementById("subsrcibe").innerHTML = "thanks for subscribing";
+ }
+
+mySubmit();
+subscribe();
